7ee24b676378d6012af6caeda4dc5607502da10f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-10 13:48:42 -0400

    bash

---
+++ requirements.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BRIGHT='\033[1m'
+NC='\033[0m' # No Color
+
+if [ "$(id -u)" -eq 0 ]; then
+    #notification message for the user to also if the local host has been copermised at all
+    title="ERROR"
+    ERROR_MESSAGE="Don't use sudo for this script. Because it can damage your computer"
+    osascript -e "display notification \"$ERROR_MESSAGE\" with title \"$title\""
+    
+    #puts the ERROR message into line art
+    echo -e "${RED}$(figlet ERROR)${NC}"
+    
+    # gives the user something to read so they understand why they got the error
+    echo "Don't use sudo for this script." 
+    echo "Because it can damage your computer"
+    exit 1
+else
+    if [[ "$1" = "--help" || "$1" = "-h" ]]; then
+        echo "This script will install the packages for it to work properly"
+    
+    else
+        # Check if the OS is macOS
+        if [[ "$OSTYPE" == "darwin"* ]]; then 
+            if ping -c 1 google.com >/dev/null 2>&1; then
+                # Packages to install
+                Packages=(
+                    "wget"
+                    "hydra"
+                    "nmap"
+                    "mysql"
+                    "figlet"
+                    "zenity"
+                )
+                
+                pipPackages=(
+                    "asyncio"
+                    "colorama"
+                    "pyfiglet"
+                )
+                
+                # Install package
+                install_Brew_package() {
+                    package_name="$1"
+                    if ! command -v "${package_name}" >/dev/null 2>&1; then
+                        echo "${package_name} is not installed. Installing..."
+                        # Replace the following command with the appropriate package manager for your Linux distribution
+                        brew install "${package_name}"
+                    else
+                        echo -e "${package_name} is already ${GREEN}installed.${NC}"
+                    fi
+                }
+
+                # Install package
+                install_pip_package() {
+                    package_name="$1"
+                    if ! python3 -m pip show "${package_name}" >/dev/null 2>&1; then
+                        echo "${package_name} is not installed. Installing..."
+                        pip3 install "${package_name}"
+                    else
+                        echo -e "${package_name} is already ${GREEN}installed.${NC}"
+                    fi
+                }
+                echo
+                echo "_________BREW PACKAGES________"
+
+                # Install BREW packages
+                for package in "${Packages[@]}"; do
+                    install_Brew_package "${package}"
+                done
+                
+                echo
+                echo "_________PIP PACKAGES________"
+                # Install PIP packages
+                for PIP in "${pipPackages[@]}"; do
+                    install_pip_package "${PIP}"
+                done
+
+                echo
+                
+                # updates PIP
+                echo "_________PIP UPDATES________"
+                /Library/Developer/CommandLineTools/usr/bin/python3 -m pip install --upgrade pip
+                
+                echo
+                successful_MESSAGE="${GREEN}[+]${NC} All packages are installed successfully"
+                echo -e "${successful_MESSAGE}"
+            else
+                echo -e "${RED}ERROR:${NC} NOT CONNECTED TO THE INTERNET"
+            fi
+
+        # Check if the OS is Linux
+        else
+            echo "[-] Wrong OS please use the correct OS." #if the user is not using the right OS, it says "You are using the wrong OS"
+        fi
+    fi
+fi
---
+++ uninstall.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# Color variables for formatting output
+RED='\033[0;31m'       # Red color
+GREEN='\033[0;32m'     # Green color
+BRIGHT='\033[1m'       # Bright text
+NC='\033[0m'           # No color
+
+# Operating system identifier for Mac
+MAC="darwin"
+
+# Check if the script is run as root (sudo)
+if [ "$(id -u)" -eq 0 ]; then
+    figlet "ERROR"  # Display an ASCII art error message
+    echo "Don't use sudo for this script." 
+    echo "Because it can damage your computer."
+    exit 1  # Exit with an error code
+else
+    # Check if the operating system is macOS
+    if [[ "$OSTYPE" == "${MAC}"* ]]; then
+        # Arrays for acceptable "yes" and "no" responses
+        yes=("yes" "Yes" "YES")
+        no=("no" "No" "NO")
+        
+        # Prompt the user to confirm if they want to remove packages
+        echo -e "${RED}${BRIGHT}!Are you sure you want to remove your Packages (YES/NO)!:${NC}"
+        read -p ">>> " YES_NO
+
+        if [[ "${yes[*]}" == *"$YES_NO"* ]]; then
+            # Check if the user is connected to the internet
+            if ping -q -c 1 -W 1 google.com >/dev/null; then 
+                # List of Brew packages that will be uninstalled if installed
+                Packages=(
+                    "wget"
+                    "hydra"
+                    "nmap"
+                    "mysql"
+                    "figlet"
+                    "zenity"
+                )
+
+                # List of PIP packages that will be uninstalled if installed
+                pipPackages=(
+                    "asyncio"
+                    "colorama"
+                )
+
+                # Function to check and uninstall a package
+                check_package() {
+                    package_name="$1"
+                    if command -v "$package_name" >/dev/null 2>&1; then
+                        echo "$package_name is installed."
+                        brew uninstall "$package_name"
+                    else
+                        echo -e "${RED}$package_name${NC} is not installed."
+                    fi
+                }
+
+                # Check and uninstall Brew packages
+                for package in "${Packages[@]}"
+                do
+                    check_package "$package"
+                done
+
+                # Uninstall PIP packages
+                for pipPackage in "${pipPackages[@]}"
+                do
+                    if pip3 show "$pipPackage" >/dev/null 2>&1; then
+                        pip3 uninstall "$pipPackage" -y
+
+                        # Check the exit status of the last command
+                        if [ $? -ne 0 ]; then
+                            echo -e "Error occurred during uninstallation of \"$pipPackage\""
+                            exit 1
+                        else
+                            echo -e "${pipPackage}: uninstalled ${GREEN}successfully${NC}"
+                        fi
+                    else
+                        echo -e "${RED}${pipPackage}${NC}: is not installed"
+                    fi
+                done
+
+                # Uninstall PIP itself
+                pip3 uninstall -y pip
+
+                # Check the exit status of the last command
+                if [ $? -ne 0 ]; then
+                    echo -e "The packages that are removed are: ${GREEN}"
+                    for package in "${Packages[@]}"
+                    do
+                        echo -e "$package"
+                    done
+                    echo -e "________PIP Packages________"
+                    for pipPackage in "${pipPackages[@]}"
+                    do
+                        echo -e "${pipPackage}"
+                    done
+
+                    echo -e "________ERROR________"
+                    echo -e "${RED}Error occurred during pip uninstallation${NC}"
+                else
+                    echo -e "pip: uninstalled ${GREEN}successfully${NC}"
+                    echo -e "The packages that are removed are: ${GREEN}"
+                    for package in "${Packages[@]}"
+                    do
+                        echo -e "${package}"
+                    done
+                    echo -e "________PIP Packages________"
+                    for pipPackage in "${pipPackages[@]}"
+                    do
+                        echo -e "${pipPackage}"
+                    done
+                fi
+            else
+                echo -e "${RED}ERROR: NOT CONNECTED TO THE INTERNET${NC}"
+            fi
+        elif [[ "${yes[*]}" == *"$YES_NO"* ]]; then
+            echo -e "[-]Ok, I will remove the packages."
+        elif [[ "${no[*]}" == *"$YES_NO"* ]];then
+            echo -e "Ok, I will not remove the packages."
+        else
+            echo -e "${RED}Please enter a valid option${NC}"
+        fi
+    else
+        echo -e "${RED}${BRIGHT}This script can only be run on Mac OS${NC}"
+    fi
+fi
